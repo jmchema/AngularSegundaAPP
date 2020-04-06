8bf8e6f90b3eda73276d990fc3ec7b831f87472d
Chema Rodriguez <jmchema@gmail.com>
2020-04-06 18:46:01 +0200

    commit despues de clase del 06042020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatefulComponent } from './stateful/stateful.component';
+import { StatelessComponent } from './stateless/stateless.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatefulComponent,
+    StatelessComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stateful/stateful.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../interface/product';
+import { Shop } from '../models/shop.model';
+
+@Component({
+  selector: 'app-stateful',
+  templateUrl: './stateful.component.html',
+  styleUrls: ['./stateful.component.css']
+})
+export class StatefulComponent implements OnInit {
+
+  shopModel: Shop = new Shop();
+  boughtItems: Array<Product>;
+
+  constructor() { 
+    this.boughtItems = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+  clickItem(curso) {
+    this.boughtItems.push(curso);
+   }
+   
+
+}
